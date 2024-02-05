6315a765d471c503e2b5970ec1d443b2af1ef514
Shahad <73951870+shahadsidek@users.noreply.github.com>
2024-02-05 15:55:49 +0300

    adding the new case study page

--- src/components/menu-bar/menu-bar.component.js
+++ src/components/menu-bar/menu-bar.component.js
@@ -21,7 +21,7 @@ class Menubar extends Component {
                                 <Nav.Link href='#aboutMe'>   About Me</Nav.Link>
                                 <Nav.Link href='#skills'>Skills</Nav.Link>
                                 <Nav.Link href='#projects'>Project</Nav.Link>
-                                <Nav.Link HREF='#case-study'>Case Study</Nav.Link>
+                                <a href="/case-study.html" target="_blank" className="nav-link">Case Study</a>
                                 <Nav.Link href='#contact'>Contact Me</Nav.Link>
                             </Nav>
                         </Navbar.Collapse>
